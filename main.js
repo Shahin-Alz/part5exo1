480b4f5ac211f4078bc71cc4fdeafafbbdbafbc4
Shahin-Alz <shaalemzadeh@gmail.com>
2023-01-21 17:25:33 +0100

    ajout

---
+++ main.js
@@ -0,0 +1,4 @@
+// Pour l'exercice 2
+let theColor = "red";
+let theBackground = "gold";
+let pContent = "Il est possible de passer des parametres dans la function utilisé dans l'écouteur d'événement !"
